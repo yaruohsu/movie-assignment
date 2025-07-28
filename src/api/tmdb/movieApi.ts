cd6a4507ac98b747aec5492163a89a45e0ccc3ea
Arrow Hsu <arrow.yr.hsu@gmail.com>
2025-07-28 16:23:22 +0800

    feat(api): add mock data

--- src/api/tmdb/movieApi.ts
+++ src/api/tmdb/movieApi.ts
@@ -1,10 +1,19 @@
 
 import { apiClient } from '@/api/apiClient';
 import { TMDB_ENDPOINTS } from '@/constants/tmdb';
+import mockSearchResult from '../__mocks__/search.json';
+import mockMovieDetail from '../__mocks__/detail.json';
 
 const language = 'en-US';
 
+const USE_MOCK = import.meta.env.VITE_USE_MOCK === 'true';
+
+
 export const searchMovies = async (data: { query: string; page: number; }) => {
+  if (USE_MOCK) {
+    return mockSearchResult;
+  }
+
   const { query, page } = data
   const res = await apiClient.get(TMDB_ENDPOINTS.SEARCH_MOVIE, {
     params: {
@@ -19,6 +28,10 @@ export const searchMovies = async (data: { query: string; page: number; }) => {
 };
 
 export const getMovieDetail = async (id: number) => {
+  if (USE_MOCK) {
+    return mockMovieDetail;
+  }
+
   const res = await apiClient.get(TMDB_ENDPOINTS.MOVIE_DETAIL(id), {
     params: {
       language
